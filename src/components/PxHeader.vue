ee5d98413865a68eab119bfebdf0403458803857
saulo <sauloco022@gmail.com>
2026-03-18 11:30:53 -0300

    config: updated to node 24

--- src/components/PxHeader.vue
+++ src/components/PxHeader.vue
@@ -2,30 +2,31 @@
   <header class="shadow">
     <nav>
       <nav class="flex items-center gap-2 flex-wrap bg-gray-900 p-6">
-        <router-link
-            :to="{ name: 'home' }"
-        >
-          <img src="../../public/favicon.png" class="h-16 w-16" height="176" width="201"/>
+        <router-link :to="{ name: 'home' }">
+          <img
+            src="../../public/favicon.png"
+            class="h-16 w-16"
+            height="176"
+            width="201"
+          />
         </router-link>
         <div>
-
           <div class="flex items-center flex-shrink-0 text-white mr-6">
             <router-link
-                :to="{ name: 'home' }"
-                class="font-semibold text-xl tracking-tight"
-            >Cryptic
-            </router-link
-            >
+              :to="{ name: 'home' }"
+              class="font-semibold text-xl tracking-tight"
+              >Cryptic
+            </router-link>
           </div>
           <div
-              class="hidden sm:block w-full flex-grow lg:flex lg:items-center lg:wauto"
+            class="hidden sm:block w-full flex-grow lg:flex lg:items-center lg:wauto"
           >
             <div class="text-sm lg:flex-grow">
               <router-link
-                  v-for="l in links"
-                  :key="l.title"
-                  :to="l.to"
-                  class="block mt-4 lg:inline-block lg:mt-0 text-blue-300 hover:text-white mr-4"
+                v-for="l in links"
+                :key="l.title"
+                :to="l.to"
+                class="block mt-4 lg:inline-block lg:mt-0 text-blue-300 hover:text-white mr-4"
               >
                 {{ l.title }}
               </router-link>
@@ -48,19 +49,19 @@ export default {
       links: [
         {
           title: "BTC",
-          to: {name: "coin-detail", params: {id: "bitcoin"}}
+          to: { name: "coin-detail", params: { id: "bitcoin" } }
         },
         {
           title: "ETH",
-          to: {name: "coin-detail", params: {id: "ethereum"}}
+          to: { name: "coin-detail", params: { id: "ethereum" } }
         },
         {
           title: "USDT",
-          to: {name: "coin-detail", params: {id: "tether"}}
+          to: { name: "coin-detail", params: { id: "tether" } }
         },
         {
           title: "XRP",
-          to: {name: "coin-detail", params: {id: "xrp"}}
+          to: { name: "coin-detail", params: { id: "xrp" } }
         }
       ]
     };
